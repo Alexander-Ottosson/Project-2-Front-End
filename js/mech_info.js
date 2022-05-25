729f42f2f6fedbdd37de1c118051b2df6b6f695c
aottosson <cMoShBd3>
2022-05-25 11:43:33 -0500

    Mech Infopage now gets populated, it gets the mech ID from the search parameter
    Mech Info page will create a link to edit the mech's info if the user is an admin
    
    Created base edit_mech.html, form has not been added to it yet

--- js/mech_info.js
+++ js/mech_info.js
@@ -7,4 +7,33 @@ async function loadMech() {
 
     const httpResponse = await fetch(url);
     const body = await httpResponse.json();
+
+    document.getElementById('modelDisplay').innerHTML = body.model;
+
+    document.getElementById('makeDisplay').innerHTML = body.make;
+
+    document.getElementById('yearDisplay').innerHTML = body.year;
+
+    document.getElementById('colorDisplay').innerHTML = body.color;
+
+    document.getElementById('speedDisplay').innerHTML = `${body.maxSpeed} km/h`
+
+    document.getElementById('weightDisplay').innerHTML = `${body.weight} kg`
+
+    document.getElementById('heightDisplay').innerHTML = `${body.height} m`
+
+    document.getElementById('descriptionDisplay').innerHTML = body.description
+
+    const userUrl = 'http://localhost:5000/current_user';
+    const userResponse = await fetch(userUrl);
+    
+    if (userResponse.status = 200) {
+        const userBody = await userResponse.json();
+
+        if (userBody.isAdmin) {
+            console.log(body);
+            document.getElementById('buttonDisplay').innerHTML = `<a href="edit_mech.html?mechId=${body.id}" class="btn btn-primary">Edit Mech</a>`;
+        }
+    }
+
 }
