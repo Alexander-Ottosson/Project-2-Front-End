0a2c0749e875f26b091bb02aa5e6a8063650c711
aottosson <cMoShBd3>
2022-05-25 11:17:39 -0500

    search.js now fills the web page properly

--- js/search.js
+++ js/search.js
@@ -35,60 +35,66 @@ async function loadMechs() {
         let mechRow = document.createElement("tr")
         tableElement.appendChild(mechRow)
 
-        let Data = document.createElement("td")
-        Data.innerHTML = mech.m_id
-        mechRow.appendChild(Data);
+        let Data2 = document.createElement("td")
+        Data2.innerHTML = mech.model
+        mechRow.appendChild(Data2);
         
         let Data5 = document.createElement("td")
         Data5.innerHTML = mech.make
         mechRow.appendChild(Data5);
 
-        let Data2 = document.createElement("td")
-        Data2.innerHTML = mech.model
-        mechRow.appendChild(Data2);
-        
         let Data7 = document.createElement("td")
         Data7.innerHTML = mech.year
         mechRow.appendChild(Data7);
-
-        let Data3 = document.createElement("td")
-        Data3.innerHTML = mech.color
-        mechRow.appendChild(Data3);
-
-        let Data8 = document.createElement("td")
-        Data8.innerHTML = mech.supervisor
-        mechRow.appendChild(Data8);
-
-        let Data4 = document.createElement("td")
-        Data4.innerHTML = mech.maxSpeed
-        mechRow.appendChild(Data4);
-
-        let Data6 = document.createElement("td")
-        Data6.innerHTML = mech.weight
-        mechRow.appendChild(Data6);
-
-        let Data9 = document.createElement("td")
-        Data9.innerHTML = mech.height
-        mechRow.appendChild(Data9);
-
-        let Data10 = document.createElement("td")
-        Data10.innerHTML = mech.description
-        mechRow.appendChild(Data10);
-
-        let Data11 = document.createElement("td")
-        Data11.innerHTML = mech.currentPilot
-        mechRow.appendChild(Data11);
-
-        let Data12 = document.createElement("td")
-        Data12.innerHTML = mech.pilotCount
-        mechRow.appendChild(Data12);
 
         let Data13 = document.createElement("td")
         Data13.innerHTML = mech.available
         mechRow.appendChild(Data13);
 
-        let Data14 = document.createElement("td")
-        Data14.innerHTML = mech.confidential
-        mechRow.appendChild(Data14);
+        let infoLink = document.createElement("td")
+        infoLink.innerHTML = `<a href="mech_info.html?mechId=${mech.id}" class="btn btn-primary">View Mech</a>`
+        mechRow.appendChild(infoLink)
+        
+        // let Data = document.createElement("td")
+        // Data.innerHTML = mech.m_id
+        // mechRow.appendChild(Data);
+
+        // let Data3 = document.createElement("td")
+        // Data3.innerHTML = mech.color
+        // mechRow.appendChild(Data3);
+
+        // let Data8 = document.createElement("td")
+        // Data8.innerHTML = mech.supervisor
+        // mechRow.appendChild(Data8);
+
+        // let Data4 = document.createElement("td")
+        // Data4.innerHTML = mech.maxSpeed
+        // mechRow.appendChild(Data4);
+
+        // let Data6 = document.createElement("td")
+        // Data6.innerHTML = mech.weight
+        // mechRow.appendChild(Data6);
+
+        // let Data9 = document.createElement("td")
+        // Data9.innerHTML = mech.height
+        // mechRow.appendChild(Data9);
+
+        // let Data10 = document.createElement("td")
+        // Data10.innerHTML = mech.description
+        // mechRow.appendChild(Data10);
+
+        // let Data11 = document.createElement("td")
+        // Data11.innerHTML = mech.currentPilot
+        // mechRow.appendChild(Data11);
+
+        // let Data12 = document.createElement("td")
+        // Data12.innerHTML = mech.pilotCount
+        // mechRow.appendChild(Data12);
+
+        // let Data14 = document.createElement("td")
+        // Data14.innerHTML = mech.confidential
+        // mechRow.appendChild(Data14);
+
+        
     });
 }
